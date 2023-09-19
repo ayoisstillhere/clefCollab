e17cde319c1a48eb38bdcbbb20767d009d2dec6f
Ayodele Fagbami <afagbami@gmail.com>
2023-09-19 14:22:17 +0100

    changing link button on navigation

--- components/shared/LeftSidebar.tsx
+++ components/shared/LeftSidebar.tsx
@@ -1,21 +1,25 @@
+"use client";
+
 import { sidebarLinks } from "@/constants";
 import Link from "next/link";
 import Image from "next/image";
+import { usePathname, useRouter } from "next/navigation";
 
-sidebarLinks;
 const LeftSideBar = () => {
+  const router = useRouter();
+  const pathname = usePathname();
   return (
     <section className="custom-scrollbar leftsidebar">
       <div className="flex w-full flex-1 flex-col gap-6 px-6">
         {sidebarLinks.map((link) => {
-          
-
-
+          const isActive =
+            (pathname.includes(link.route) && link.route.length > 1) ||
+            pathname === link.route;
           return (
             <Link
               href={link.route}
               key={link.label}
-              className="leftsidebar_link"
+              className={`leftsidebar_link ${isActive && "bg-primary-500"}`}
             >
               <Image
                 src={link.imgURL}
