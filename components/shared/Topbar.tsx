670451036057e0005bab5e83570a51208b0b08bd
Ayodele Fagbami <afagbami@gmail.com>
2023-09-19 10:39:34 +0100

    navbar layout

--- components/shared/Topbar.tsx
+++ components/shared/Topbar.tsx
@@ -1,9 +1,33 @@
 import Link from "next/link";
+import Image from "next/image";
+import { OrganizationSwitcher, SignOutButton, SignedIn } from "@clerk/nextjs";
 
 const Topbar = () => {
-  return(
+  return (
     <nav className="topbar">
-      <Link></Link>
+      <Link href="/" className="flex items-center gap-4">
+        <Image src="/logo.svg" alt="logo" width={38} height={38} />
+        <p className="text-heading3-bold text-light-1 max-xs:hidden">
+          ClefCollab
+        </p>
+      </Link>
+      <div className="flex items-center gap-1">
+        <div className="block md:hidden">
+          <SignedIn>
+            <SignOutButton>
+              <div className="flex cursor-pointer">
+                <Image
+                  src="/assets/logout.svg"
+                  alt="logout"
+                  width={24}
+                  height={24}
+                />
+              </div>
+            </SignOutButton>
+          </SignedIn>
+        </div>
+        <OrganizationSwitcher appearance={{elements: {organizationSwitcherTrigger: "py-2 px-4"}}}/>
+      </div>
     </nav>
   );
 };
