db8d69eebb8e13aae6bad07d6d4d077abddcb155
Ayodele Fagbami <afagbami@gmail.com>
2023-09-20 10:20:31 +0100

    update user method

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -1,7 +1,30 @@
-"use server"
+"use server";
 
-import { connectToDB } from "../mongoose"
+import { revalidatePath } from "next/cache";
+import User from "../models/user.model";
+import { connectToDB } from "../mongoose";
 
-export async function updateUser(): Promise<void> {
-    connectToDB();
+export async function updateUser(
+  userId: string,
+  username: string,
+  name: string,
+  bio: string,
+  image: string,
+  path: string
+): Promise<void> {
+  connectToDB();
+
+  try {
+    await User.findOneAndUpdate(
+      { id: userId },
+      { username: username.toLowerCase(), name, bio, image, onboarded: true },
+      { upsert: true } // create if not exists, updates if exists
+    );
+
+    if (path === "/profile/edit") {
+      revalidatePath(path);
+    }
+  } catch (error: any) {
+    throw new Error(`Failed to create/update user: ${error.message}`);
+  }
 }
