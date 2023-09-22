0bd81659a0a034199a952d68727853f462124288
Ayodele Fagbami <afagbami@gmail.com>
2023-09-22 12:22:41 +0100

    remove unnecessary comment

--- components/forms/PostThread.tsx
+++ components/forms/PostThread.tsx
@@ -16,7 +16,6 @@ import { Textarea } from "../ui/textarea";
 
 import { usePathname, useRouter } from "next/navigation";
 
-// import { updateUser } from "@/lib/actions/user.actions";
 import { ThreadValidation } from "@/lib/validations/thread";
 import { createThread } from "@/lib/actions/thread.actions";
 
