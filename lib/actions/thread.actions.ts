ecd412d0a2498c96281c7205c747bfe9fbbd350b
Ayodele Fagbami <afagbami@gmail.com>
2023-09-22 15:58:20 +0100

    fetch user threads method

--- lib/actions/thread.actions.ts
+++ lib/actions/thread.actions.ts
@@ -148,3 +148,28 @@ export async function addCommentToThread(
     throw new Error(`Error adding comment to thread: ${error.message}`);
   }
 }
+
+export async function fetchUserThreads(userId: string) {
+  try {
+    connectToDB();
+
+    //Find all threads authored by the user with the given UID
+    // TODO: Populate with community
+    const threads = await User.findOne({ id: userId }).populate({
+      path: "threads",
+      model: Thread,
+      populate: {
+        path: "children",
+        model: Thread,
+        populate: {
+          path: "author",
+          model: User,
+          select: "name image id",
+        },
+      },
+    });
+    return threads;
+  } catch (error: any) {
+    throw new Error(`Failed to fetch user threads: ${error.message}`);
+  }
+}
